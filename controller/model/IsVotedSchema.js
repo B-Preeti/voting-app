1118330a7bb9d464d8a38d13918ce861c2b37869
B-Preeti15 <137649724+B-Preeti@users.noreply.github.com>
2025-01-23 22:53:39 +0530

    Delete controller directory

--- controller/model/IsVotedSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose");
-const IsVotedSchema=new mongoose.Schema({
-    "Id":{type:String},
-    "PartyVoted":{type:String},
-},{
-    collection:"IsVoted"
-})
-
-module.exports=mongoose.model("IsVotedSchema",IsVotedSchema);
